e53b283fb5f489a94681f5d293832490656562c9
shodan993 <paulnstanley@gmail.com>
2018-09-28 16:47:59 -0400

    simplifying server/index.js

--- receipty/server/index.js
+++ receipty/server/index.js
@@ -4,12 +4,6 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
-require('./models/User');
-require('./models/Survey');
-require('./services/passport');
-require('./routes/billingRoutes');
-require('./routes/surveyRoutes')(app);
-
 
 mongoose.connect(keys.mongoURI)
 
@@ -25,10 +19,6 @@ app.use(
 )
 app.use(passport.initialize())
 app.use(passport.session())
-
-
-require('./routes/authRoutes')(app)
-require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
