02d3bcddb28644ed7e8b5d5e7e688fb8bf49205d
zulmycortez <zulmycortez@gmail.com>
2018-09-28 22:15:01 -0400

    Added actions, components, and reducer folders and some basics for App component

---
+++ receipty/server/client/src/actions/index.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { FETCH_USER, FETCH_EXPENSES, FETCH_REPORTS } from './types';
+
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get('/current_user');
+  
+    dispatch({ type: FETCH_USER, payload: res.data });
+  };
+  
+  export const submitExpenses = (values, history) => async dispatch => {
+    const res = await axios.post('/expenses', values);
+  
+    history.push('/expenses');
+    dispatch({ type: FETCH_USER, payload: res.data });
+  };
+
+  export const submitReports = (values, history) => async dispatch => {
+    const res = await axios.post('/reports', values);
+  
+    history.push('/reports');
+    dispatch({ type: FETCH_USER, payload: res.data });
+  };
+  
+  export const fetchExpenses = () => async dispatch => {
+    const res = await axios.get('/expenses');
+  
+    dispatch({ type: FETCH_EXPENSES, payload: res.data });
+  };
+
+  export const fetchReports = () => async dispatch => {
+    const res = await axios.get('/reports');
+  
+    dispatch({ type: FETCH_REPORTS, payload: res.data });
+  };
+  
---
+++ receipty/server/client/src/actions/types.js
@@ -0,0 +1,3 @@
+export const FETCH_USER = 'fetch_user';
+export const FETCH_EXPENSES = 'fetch_expenses';
+export const FETCH_REPORTS = 'fetch_reports'
