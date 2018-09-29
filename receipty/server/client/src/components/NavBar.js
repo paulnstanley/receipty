b788b42dd432ef0186d3ba6366a267a1be75c622
Carrington Perry <carringtonperry@Carringtons-MacBook-Pro.local>
2018-09-29 17:17:45 -0400

    backing up to git

--- receipty/server/client/src/components/NavBar.js
+++ receipty/server/client/src/components/NavBar.js
@@ -15,6 +15,9 @@ class NavBar extends Component {
       
 
       <ul className = "navbar-ul ">
+
+
+         {/* links to all major routes. do note that we agreed that for mvp Reports, Inbox, and Logout are all duds. I put settings so that it looks nice.no need to make it do anything, just don't click it.  */}
         <li><a className="navbar-li-a-active navbar-li-a">Expenses</a></li>
         <li><a className = "navbar-li-a">Reports</a></li>
         <li><a className = "navbar-li-a">Inbox</a></li>
