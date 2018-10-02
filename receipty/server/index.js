6afb3a8a0d3ba5b9e7864999700366a8f9f016e3
Carrington Perry <carringtonperry@carringtons-mbp.lan>
2018-10-02 11:47:01 -0400

    fixing cors errors

--- receipty/server/index.js
+++ receipty/server/index.js
@@ -4,6 +4,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 require('./models/User');
 
@@ -11,6 +12,7 @@ mongoose.connect(keys.mongoURI)
 
 const app = express()
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
