84cdb27a10e069d639ed0a24dfccdd05234f4dee
Lindsay Schinasi <lindsay.schinasi@gmail.com>
2018-10-04 14:09:49 -0400

    fixing expenseform compile errors

--- receipty/server/client/src/components/expensesNew/ExpenseForm.js
+++ receipty/server/client/src/components/expensesNew/ExpenseForm.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import formFields from './formFields';
+// import withRouter from 'react';
+import _ from 'lodash';
 
 import NavBar from '../navbar/NavBar'
 class ExpenseForm extends Component {
@@ -10,7 +12,7 @@ class ExpenseForm extends Component {
       return (
         <Field
           key={name}
-          component={formFieldsStyle}
+          component={formFields}
           type="text"
           label={label}
           name={name}
@@ -39,83 +41,83 @@ class ExpenseForm extends Component {
 }
 
 
-const options = [
-  { value: 'travel', label: 'Travel', className: 'travel' },
-  { value: 'food', label: 'Food', className: 'food' },
-  { value: 'supplies', label: 'Supplies', className: 'supplies'}, 
-  { value: 'utilities', label: 'Utilities', className: 'utilities'}, 
-  { value: 'entertainment', label: 'Entertainment', className: 'entertainment'}, 
-  { value: 'other', label: 'Other', className: 'other'}
-];
+// const options = [
+//   { value: 'travel', label: 'Travel', className: 'travel' },
+//   { value: 'food', label: 'Food', className: 'food' },
+//   { value: 'supplies', label: 'Supplies', className: 'supplies'}, 
+//   { value: 'utilities', label: 'Utilities', className: 'utilities'}, 
+//   { value: 'entertainment', label: 'Entertainment', className: 'entertainment'}, 
+//   { value: 'other', label: 'Other', className: 'other'}
+// ];
 
-const defaultOption = options[0];
+// const defaultOption = options[0];
 
-const ExpenseForm = props => {
+// const ExpenseForm = props => {
   
-  const { handleSubmit, pristine, reset, submitting } = props
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Merchant Name</label>
-        <div>
-          <Field
-            name="Name"
-            component="input"
-            type="text"
-            placeholder="Merchant Name"
-          />
-        </div>
-      </div>
-      <div>
-        <label>Amount</label>
-        <div>
-          <Field
-            name="Amount"
-            component="input"
-            type="text"
-            placeholder="Amount"
-          />
-        </div>
-      </div>
-      <div>
-        <label>Date</label>
-        <div>
-        <Field
-            name="Date"
-            component="input"
-            type="text"
-            placeholder="Date"
-          />
-        </div>
-      </div>
-      <div>
-        <label>Categories</label>
-        <div>
-        <Field
-            name="Categories"
-            component="input"
-            type="text"
-            placeholder="Categories"
-          />
-        </div>
-      </div>
-      <div>
-        <label>Comments</label>
-        <div>
-          <Field name="Comments" component="textarea" />
-        </div>
-      </div>
-      <div>
-        <button type="submit" disabled={pristine || submitting}>
-          Submit
-        </button>
-        <button type="button" disabled={pristine || submitting} onClick={reset}>
-          Clear Values
-        </button>
-      </div>
-    </form>
-  )
-}
+//   const { handleSubmit, pristine, reset, submitting } = props
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div>
+//         <label>Merchant Name</label>
+//         <div>
+//           <Field
+//             name="Name"
+//             component="input"
+//             type="text"
+//             placeholder="Merchant Name"
+//           />
+//         </div>
+//       </div>
+//       <div>
+//         <label>Amount</label>
+//         <div>
+//           <Field
+//             name="Amount"
+//             component="input"
+//             type="text"
+//             placeholder="Amount"
+//           />
+//         </div>
+//       </div>
+//       <div>
+//         <label>Date</label>
+//         <div>
+//         <Field
+//             name="Date"
+//             component="input"
+//             type="text"
+//             placeholder="Date"
+//           />
+//         </div>
+//       </div>
+//       <div>
+//         <label>Categories</label>
+//         <div>
+//         <Field
+//             name="Categories"
+//             component="input"
+//             type="text"
+//             placeholder="Categories"
+//           />
+//         </div>
+//       </div>
+//       <div>
+//         <label>Comments</label>
+//         <div>
+//           <Field name="Comments" component="textarea" />
+//         </div>
+//       </div>
+//       <div>
+//         <button type="submit" disabled={pristine || submitting}>
+//           Submit
+//         </button>
+//         <button type="button" disabled={pristine || submitting} onClick={reset}>
+//           Clear Values
+//         </button>
+//       </div>
+//     </form>
+//   )
+// }
 
 export default reduxForm({
   form: 'simple' // a unique identifier for this form
