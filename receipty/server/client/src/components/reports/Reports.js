26797ebf0e5dcae2c6c8c10275c4c326c1da5d4f
amhayslip <amhayslip@gmail.com>
2018-10-15 14:05:49 -0400

    suggestions

--- receipty/server/client/src/components/reports/Reports.js
+++ receipty/server/client/src/components/reports/Reports.js
@@ -1,32 +1,50 @@
-//this file will create what will be the Reports page. 
-//required node modules
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import ReportListTable from './ReportListTable.js';
-//required components to create the Reports components.
-import NavBar from '../navbar/NavBar';
+import { bindActionCreators } from "redux";
+import { connect } from 'react-redux';
+import { Button } from "react-bootstrap";
+import NavBar from '../navbar/NavBar.js';
+import { withRouter } from 'react-router-dom';
+import { fetchReports } from '../../actions';
 
-// component that will be rendered as main part of Report 
-class Reports extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar />
-        <ReportListTable />
-        <div className="fixed-action-btn">
-          <Link to="/reports/new" className="teal btn-flat right white-text">
-            New Report
-          </Link>
-          <div className="report-container">
-            {/* <span>{this.props}</span>
-            <span>{this.props}</span>
-            <span>{this.props}</span>
-            <span>{this.props}</span> */}
-          </div>
-        </div>
-      </div>
-    );
+class Reports extends Component {  
+  constructor(props) {
+    super(props);
+  }
+     
+  componentDidMount() {
+    this.props.fetchReports();
   };
+    
+  render () {
+    if (!this.props.reports) {
+      return (
+        <div>Loading</div>
+      )
+    } else { 
+      return (
+        <div>
+          <NavBar />
+          <ul>
+            { this.props.reports.map((report) => {
+              return (
+                <li key={report._id}>Report Name: {report.name} </li>
+              )
+            })}
+          </ul>
+        </div>   
+      )
+    }
+  }
 }
 
-export default Reports;
+ //this maps our state according to expesnse and connects with our fetch Expenses function and exports as Expense List//
+function mapStateToProps({ reports }) {
+  return { reports };
+}
+
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ fetchReports }, dispatch);
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Reports))
